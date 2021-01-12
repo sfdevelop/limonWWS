76fae859b7660c3d9ac1dee35a3806dd7dfe1933
sfdevelop <sfdevelop747@gmail.com>
2021-01-12 20:32:03 +0200

    12-01

--- assets/js/main.js
+++ assets/js/main.js
@@ -17767,3 +17767,17 @@ $(document).ready(function () {
 		autoplaySpeed: 6000
 	});
 });
+
+//боковое  меню
+$(document).ready(function () {
+	$("#navToggle").click(function () {
+		$(this).toggleClass("active");
+		$(".overlay").toggleClass("open");
+		// this line ▼ prevents content scroll-behind
+		$("body").toggleClass("locked");
+	});
+	$('.mobile_li').click(function (ev) {
+		$(this).find('>ul').slideToggle();
+		ev.stopPropagation();
+	});
+});
